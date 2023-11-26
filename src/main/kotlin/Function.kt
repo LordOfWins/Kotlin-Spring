c27562b5334712b2cf3d24129b1bb21ff96f3c70
윤승재 <sjyun011278@gmail.com>
2023-11-26 20:11:36 +0900

    Default Parameters #4

--- src/main/kotlin/Function.kt
+++ src/main/kotlin/Function.kt
@@ -1,5 +1,7 @@
 package org.springkotlin
 
+import java.time.LocalDate
+
 fun printName(name: String) {
     println("Name is $name")
 }
@@ -10,6 +12,12 @@ fun addition(x: Int, y: Int): Int {
 
 fun additionApproach(x: Int, y: Int) = x + y
 
+fun defaultParameters(name: String, email: String = "", dob: LocalDate = LocalDate.now()) {
+
+    println("Name is $name and the email is $email and the dob is $dob")
+}
+
+
 fun main() {
     val unit = printName("seungjae")
     println(unit)
@@ -17,4 +25,9 @@ fun main() {
     println("Result is $result")
     val result2 = additionApproach(2, 3)
     println("Result is $result2")
+
+    defaultParameters("Dilip", "abc@gmail.com", LocalDate.parse("2000-01-01"))
+    defaultParameters("Dilip")
+    defaultParameters("Dilip", dob = LocalDate.parse("2000-01-01"))
+    defaultParameters(dob = LocalDate.parse("2000-01-01"), name = "Dilip", email = "abc@gmail.com")
 }
