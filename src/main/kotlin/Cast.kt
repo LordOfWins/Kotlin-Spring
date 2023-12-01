2ab5ca2f44cbf94d222a27a5204fd76b757eb4f0
윤승재 <sjyun011278@gmail.com>
2023-12-01 22:12:23 +0900

    casting #13

---
+++ src/main/kotlin/Cast.kt
@@ -0,0 +1,38 @@
+package org.springkotlin
+
+fun main() {
+    val course = Course(
+        1,
+        "Reactive Programming in Modern Java using Project Reactor",
+        "DILIP"
+    )
+    checkType(course)
+    checkType("SEUNGJAE")
+
+    castNumber(1.0)
+//    castNumber(1)
+
+    val number = 1
+    val numberDouble = number.toDouble()
+    println(numberDouble)
+}
+
+fun castNumber(any: Any) {
+    when (any) {
+        any as Double -> {
+            println(any)
+        }
+    }
+}
+
+fun checkType(type: Any) {
+    when {
+        type is Course -> {
+            println(type.copy())
+        }
+
+        type is String -> {
+            println(type.lowercase())
+        }
+    }
+}
