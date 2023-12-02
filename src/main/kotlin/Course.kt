4e6776dfd60d99f543479f6da0515a53a2caacf1
윤승재 <sjyun011278@gmail.com>
2023-12-02 21:25:36 +0900

    Enum #14

--- src/main/kotlin/Course.kt
+++ src/main/kotlin/Course.kt
@@ -4,17 +4,36 @@ data class Course(
     val id: Int,
     val name: String,
     val author: String,
+    var courseCategory: CourseCategory = CourseCategory.DEVELOPEMENT
 )
 
+enum class CourseCategory {
+    DEVELOPEMENT,
+    BUSINESS,
+    DESIGN,
+    MARKETING
+}
+
 fun main() {
-    val course = Course(1, "Kotlin", "seungjae")
+
+    val course = Course(1, "Reactive Programming in Modern Java using Project Reactor", "Dilip")
+
     println(course)
-    val course2 = Course(1, "Kotlin", "seungjae")
-    println(course2 == course)
 
-    val course3 = Course(2, "Kotlin", "seungjae")
-    println(course3 == course)
+    val course1 = Course(1, "Reactive Programming in Modern Java using Project Reactor", "Dilip")
 
-    val course4 = course.copy(id = 2, name = "Spring", author = "Alex")
-    println(course4)
+    println("Checking ~")
+
+    val course3 = course1.copy(
+
+        id = 3, author = "D~"
+
+    )
+
+    println(course3)
+
+    val marketingCourse = Course(1, "FaceBook Marketing", "Dilip", CourseCategory.MARKETING)
+
+    println(marketingCourse)
+
 }
