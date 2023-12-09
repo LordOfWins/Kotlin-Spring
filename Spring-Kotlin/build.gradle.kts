b0e689f38cc2d2342106e6ed49b260163632d69d
윤승재 <sjyun011278@gmail.com>
2023-12-09 23:22:27 +0900

    Kotlin-Spring init #21

---
+++ Spring-Kotlin/build.gradle.kts
@@ -0,0 +1,60 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
+    kotlin("plugin.jpa") version "1.9.21"
+}
+
+group = "org.springkotlin"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // https://mvnrepository.com/artifact/io.github.microutils/kotlin-logging
+    val kLogging = "3.0.5"
+    implementation("io.github.microutils:kotlin-logging:$kLogging")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.h2database:h2")
+//    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+    val mokk = "1.13.4"
+    testImplementation("io.mockk:mockk:$mokk")
+    val ninja = "4.0.2"
+    testImplementation("com.ninja-squad:springmockk:${ninja}")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "21"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+sourceSets {
+    test {
+        java {
+            setSrcDirs(listOf("src/test/intg", "src/test/unit"))
+        }
+
+    }
+}
