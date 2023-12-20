a7fee28fd09af08005bdc0a2546aeb5e05c6e1d1
윤승재 <sjyun011278@gmail.com>
2023-12-20 21:06:06 +0900

    CourseService #32

---
+++ Spring-Kotlin/src/main/kotlin/org/springkotlin/springkotlin/service/CourseService.kt
@@ -0,0 +1,69 @@
+package org.springkotlin.springkotlin.service
+
+import mu.KLogging
+import org.springframework.stereotype.Service
+import org.springkotlin.springkotlin.dto.CourseDTO
+import org.springkotlin.springkotlin.entity.Course
+import org.springkotlin.springkotlin.exception.CourseNotFoundException
+import org.springkotlin.springkotlin.repository.CourseRepository
+
+@Service
+class CourseService(val courseRepository: CourseRepository) {
+
+    companion object : KLogging()
+
+    fun addCourse(courseDTO: CourseDTO): CourseDTO {
+        val course = courseDTO.let {
+            Course(id = null, name = it.name, category = it.category)
+        }
+        courseRepository.save(course)
+
+        logger.info("Course saved: $course")
+        return course.let {
+            CourseDTO(id = it.id, name = it.name, category = it.category)
+        }
+    }
+
+
+    fun getCourses(): List<CourseDTO> {
+        return courseRepository.findAll().map { CourseDTO(id = it.id, name = it.name, category = it.category) }
+    }
+
+    fun updateCourse(id: Int, courseDTO: CourseDTO): CourseDTO {
+        val course = courseRepository.findById(id)
+        return if (course.isPresent) {
+            course.get().let {
+                it.name = courseDTO.name
+                it.category = courseDTO.category
+                courseRepository.save(it)
+                CourseDTO(id = it.id, name = it.name, category = it.category)
+            }
+        } else {
+            throw CourseNotFoundException("Course not found with id: $id")
+        }
+    }
+
+    fun deleteCourse(id: Int) {
+        val course = courseRepository.findById(id)
+        return if (course.isPresent) {
+            course.get().let {
+                courseRepository.deleteById(id)
+            }
+        } else {
+            throw CourseNotFoundException("Course not found with id: $id")
+        }
+    }
+
+    fun updateName(id: Int, name: String): CourseDTO {
+        val course = courseRepository.findById(id)
+        return if (course.isPresent) {
+            course.get().run {
+                this.name = name
+                courseRepository.save(this)
+                CourseDTO(id = this.id, name = this.name, category = this.category)
+            }
+        } else {
+            throw CourseNotFoundException("Course not found with id: $id")
+        }
+    }
+}
