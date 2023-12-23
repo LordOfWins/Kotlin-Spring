21eb6d1bb26621afa1c1093590c612ffe7725d26
윤승재 <sjyun011278@gmail.com>
2023-12-23 22:14:12 +0900

    TestUtil #35

---
+++ Spring-Kotlin/src/test/intg/org/springkotlin/springkotlin/util/TestUtil.kt
@@ -0,0 +1,18 @@
+package org.springkotlin.springkotlin.util
+
+import org.springkotlin.springkotlin.entity.Course
+
+fun courseEntityList() = listOf(
+    Course(
+        null,
+        "Build RestFul APis using SpringBoot and Kotlin", "Development"
+    ),
+    Course(
+        null,
+        "Build Reactive Microservices using Spring WebFlux/SpringBoot", "Development",
+    ),
+    Course(
+        null,
+        "Wiremock for Java Developers", "Development",
+    )
+)
