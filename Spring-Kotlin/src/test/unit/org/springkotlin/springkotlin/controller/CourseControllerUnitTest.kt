2977c1426aef814187534d51e24c90de5046f209
윤승재 <sjyun011278@gmail.com>
2023-12-24 20:54:27 +0900

    CourseControllerUnitTest #36

---
+++ Spring-Kotlin/src/test/unit/org/springkotlin/springkotlin/controller/CourseControllerUnitTest.kt
@@ -0,0 +1,79 @@
+package org.springkotlin.springkotlin.controller
+
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.just
+import io.mockk.runs
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springkotlin.springkotlin.dto.CourseDTO
+import org.springkotlin.springkotlin.service.CourseService
+import org.springkotlin.springkotlin.util.courseDTO
+
+@WebMvcTest(controllers = [CourseController::class])
+@AutoConfigureWebTestClient
+class CourseControllerUnitTest {
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
+
+    @MockkBean
+    private lateinit var courseService: CourseService
+
+    @Test
+    fun addCourse() {
+        //given
+        val courseDTO = CourseDTO(null, name = "test", category = "test")
+        every { courseService.addCourse(any()) } returns courseDTO(id = 1)
+        val savedCourseDTO =
+            webTestClient.post().uri("/v1/courses").bodyValue(courseDTO).exchange().expectStatus().isCreated.expectBody(
+                CourseDTO::class.java
+            ).returnResult().responseBody
+        assertTrue {
+            savedCourseDTO!!.id != null
+        }
+    }
+
+    @Test
+    fun getCourses() {
+        every { courseService.getCourses() }.returnsMany(
+            listOf(
+                courseDTO(id = 1),
+                courseDTO(id = 2),
+                courseDTO(id = 3)
+            )
+        )
+        val courses = webTestClient.get().uri("/v1/courses").exchange().expectStatus().is2xxSuccessful.expectBodyList(
+            CourseDTO::class.java
+        ).returnResult().responseBody
+        Assertions.assertEquals(3, courses!!.size)
+    }
+
+    @Test
+    fun updateCourse() {
+        every { courseService.updateCourse(any(), any()) } returns courseDTO(
+            id = 100,
+            name = "sdfsdf",
+            category = "sdfsdf"
+        )
+        val updatedCourseDTO = CourseDTO(null, "sdfsdf", "sdfsdf")
+
+        val updatedCourse =
+            webTestClient.put().uri("/v1/courses/{id}", 100).bodyValue(updatedCourseDTO).exchange()
+                .expectStatus().isOk.expectBody(
+                    CourseDTO::class.java
+                ).returnResult().responseBody
+        Assertions.assertEquals("sdfsdf", updatedCourse!!.name)
+    }
+
+    @Test
+    fun deleteCourse() {
+        every { courseService.deleteCourse(any()) } just runs
+        webTestClient.delete().uri("/v1/courses/{id}", 100).exchange().expectStatus().isNoContent
+    }
+
+}
