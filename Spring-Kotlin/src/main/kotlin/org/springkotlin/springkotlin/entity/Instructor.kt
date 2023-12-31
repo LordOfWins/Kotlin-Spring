a76e90e850824fcafcb9625387d1a7cb32d91399
윤승재 <sjyun011278@gmail.com>
2023-12-31 21:14:21 +0900

    Instructor #43

---
+++ Spring-Kotlin/src/main/kotlin/org/springkotlin/springkotlin/entity/Instructor.kt
@@ -0,0 +1,17 @@
+package org.springkotlin.springkotlin.entity
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "INSTRUCTOR")
+data class Instructor(
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Int?,
+    var name: String,
+    @OneToMany(
+        mappedBy = "instructor",
+        cascade = [CascadeType.ALL],
+        orphanRemoval = true
+    )
+    var courses: List<Course> = mutableListOf()
+)
